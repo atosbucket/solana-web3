d8c0620b19f7fbd6be7f7c3186977e545608d87b
Atos <atos@Atoss-Mac.local>
2024-04-03 20:10:11 -0700

    init

---
+++ generate-keypair.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Keypair } from "@solana/web3.js";
+
+// const keypair = Keypair.generate();
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
